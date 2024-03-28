021d3d7c254f169bc18c9f0a378d574a62dab2e7
Nicholas Leite <nicholas.lopes@plugg.to>
2024-03-28 02:19:34 -0300

    Breaking in smaller components

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,36 @@
+<template>
+    <li class="list-group-item py-3">
+        <div class="d-flex justify-content-start align-items-center">
+            <input class="form-check-input mt-0" :class="completedTaskClass" type="checkbox" :checked="task.is_completed" />
+            <div class="ms-2 flex-grow-1" :class="completedTaskClass" title="Double click the text to edit or remove">
+                <!-- <div class="relative">
+                                            <input class="editable-task" type="text" />
+                                        </div> -->
+                <span>{{ task.name }}</span>
+            </div>
+            <!-- <div class="task-date">24 Feb 12:00</div> -->
+        </div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+                <IconPencil />
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger">
+                <IconTrash />
+            </button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+
+import { computed } from 'vue';
+import IconPencil from '../icons/IconPencil.vue';
+import IconTrash from '../icons/IconTrash.vue';
+
+const props = defineProps({
+    task: Object,
+});
+
+const completedTaskClass = computed(() => (props.task.is_completed)? 'completed' : '');
+
+</script>
---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card mt-2" v-if="tasks.length && show">
+        <ul class="list-group list-group-flush">
+            <Task v-for="task in tasks" :key="task.id" :task="task" />
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import Task from "./Task.vue";
+
+const props = defineProps({
+    tasks: Array,
+    show: {
+        type: Boolean,
+        default: true
+    },
+});
+</script>
