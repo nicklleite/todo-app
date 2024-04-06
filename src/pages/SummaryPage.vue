6f35e97304544f3728954dcd0848ad32593564a2
Nicholas Leite <nicholas.lopes@plugg.to>
2024-04-06 04:52:50 -0300

    Finished the course! Now the fun begins!

--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -1,7 +1,57 @@
 <template>
     <main style="min-height: 50vh; margin-top: 2rem">
         <div class="container">
-            <h1>Summary</h1>
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div
+                        class="d-flex justify-content-between align-items-center mb-4"
+                    >
+                        <h1>
+                            Summary
+                            <small class="text-muted fs-4">{{
+                                selectedFilter.text
+                            }}</small>
+                        </h1>
+                        <SummaryFilter @update="setSelectedFilter" />
+                    </div>
+                    <div
+                        v-for="(tasks, description) in summaries"
+                        :key="description"
+                    >
+                        <Summaries :tasks="tasks" :description="description" />
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </template>
+
+<script setup>
+import { useSummaryStore } from "../stores/summary";
+import { onMounted, reactive, watch } from "vue";
+// import { useRoute } from "vue-router";
+import { storeToRefs } from "pinia";
+import Summaries from "../components/summaries/Summaries.vue";
+import SummaryFilter from "../components/summaries/filter/SummaryFilter.vue";
+
+const store = useSummaryStore();
+const { summaries } = storeToRefs(store);
+const { fetchTasksSummary } = store;
+const selectedFilter = reactive({
+    period: "",
+    text: "",
+});
+
+const setSelectedFilter = (event) => Object.assign(selectedFilter, event);
+
+onMounted(async () => {
+    await fetchTasksSummary();
+});
+
+// const route = useRoute()
+// watch(() => route.query, async (query) => await fetchTasksSummary(query))
+watch(
+    () => selectedFilter.period,
+    async (period) => await fetchTasksSummary({ period }),
+);
+</script>
